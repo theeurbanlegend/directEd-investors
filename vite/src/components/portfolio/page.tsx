a6618dd296fb3af659a755096d50bb235c6d448f
Ray-Ezra <raymondezra276@gmail.com>
2024-04-29 13:04:15 +0300

    Profile updates

--- vite/src/components/portfolio/page.tsx
+++ vite/src/components/portfolio/page.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+import { useUserContext } from "../../context/userContext";
 import PortfolioPage from "../portfolio";
 import LandingLayout from "./layout";
 
 const Portfolio = () => {
+	const {name, getCurrentUser}=useUserContext()
+	
+	useEffect(()=>{
+		getCurrentUser()
+	}, [])
+
   const investments = [
     {
       pool: "Pool A",
@@ -28,14 +36,13 @@ const Portfolio = () => {
 
   const totalInvestment = investments.reduce((total, investment) => total + investment.amount, 0);
   const tokenBalance = investments.reduce((totalTokens, investment) => totalTokens + investment.tokensEarned, 0);
-  const userName = 'Raymond';
 
   return (
     <LandingLayout>
       <div className="flex items-center justify-center">
         <div className="flex flex-col items-center w-full max-w-screen-lg p-6 overflow-y-auto">
           <div className="flex items-center justify-center flex-col mb-10">
-            <h1 className="text-4xl md:text-5xl font-bold text-center mb-10">Welcome, {userName}!</h1>
+            <h1 className="text-4xl md:text-5xl font-bold text-center mb-10">Welcome, {name}!</h1>
             <p className="text-lg text-center font-bold">Your Investments</p>
             {/* Additional personalized content here */}
           </div>
