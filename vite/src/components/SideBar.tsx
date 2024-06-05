13968d0711296406f2106b2c80949a2f7762d5a9
itsrandyM <randymungai20@gmail.com>
2024-06-05 12:47:30 +0300

    new icons and fixed width

--- vite/src/components/SideBar.tsx
+++ vite/src/components/SideBar.tsx
@@ -1,37 +1,38 @@
 import {
-	DashboardIcon,
-	EnvelopeClosedIcon,
 	GearIcon,
-	LightningBoltIcon,
 	PersonIcon,
 } from "@radix-ui/react-icons";
+import { RiBriefcaseLine,  RiCoinsLine,RiCoinsFill} from "react-icons/ri";
+import { MdDashboard } from "react-icons/md";
+import { IoBulbOutline, IoBulb  } from "react-icons/io5"
+import { FaPen } from "react-icons/fa";
 import { Link, useLocation }from "react-router-dom";
 
 const TopLinks = [
 	{
 		title: "Dashboard",
 		href: "/dashboard",
-		icon: <DashboardIcon />,
+		icon: <MdDashboard />,
 	},
 	{
 		title: "Portfolio",
 		href: "/portfolio",
-		icon: <DashboardIcon />,
+		icon: <RiBriefcaseLine />,
 	},
 	{
 		title: "Pools",
 		href: "/pools",
-		icon: <EnvelopeClosedIcon />,
+		icon: < RiCoinsFill />,
 	},
 	{
 		title: "Analytics",
 		href: "#",
-		icon: <LightningBoltIcon />,
+		icon: <IoBulb/>,
 	},
 	{
 		title: "Create",
 		href: "/create",
-		icon: <LightningBoltIcon />,
+		icon: <FaPen />,
 	},
 ];
 
@@ -65,10 +66,11 @@ const Sidebar = () => {
 				</Link>
 				<div className="flex flex-col items-start gap-6 ">
 					{TopLinks.map((link, index) => (
-						<Link to={link.href} key={index}>
+						<Link to={link.href} key={index} className="w-full">
 							<div
 								key={index}
-								className={`flex items-center gap-3 cursor-pointer px-4 ${isActiveLink(link.href) ? ' text-[#395241] font-semibold border-l border-[#395241] shadow-md' : ''} hover:underline`}
+								className={`flex items-center gap-3 cursor-pointer px-4 ${isActiveLink(link.href) ? 'text-[#395241] font-semibold border-l border-[#395241] shadow-md' : ''} 
+								hover:shadow-sm`}
 							>
 								<div>{link.icon}</div>
 								<h2 className="md:text-lg">{link.title}</h2>
